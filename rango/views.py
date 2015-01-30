073a22889a3a24a90ef17823bea4e25265eaa7ab
Ross Hartley <mrh0867@gmail.com>
2015-01-29 22:40:32 -0500

    added first app

---
+++ rango/views.py
@@ -0,0 +1,9 @@
+# Create your views here.
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Rango says Hello World! <a href='/rango/about/'>About</a>")
+
+def about(request):
+    return HttpResponse("Rango Says: Here is the about page. <a href='/rango'>Index</a>")
