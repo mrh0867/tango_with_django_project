c8b6241c1307a11392241674d2ce72027e4db88d
Ross Hartley <mrh0867@gmail.com>
2015-02-01 22:45:01 -0500

    finished chapter 5

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,8 +1,9 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -15,5 +16,13 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^rango', include('rango.urls')),
+    url(r'^rango/', include('rango.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static',
+        (r'media/(?P<path>.*)',
+         'serve',
+         {'document_root': settings.MEDIA_ROOT}), )
